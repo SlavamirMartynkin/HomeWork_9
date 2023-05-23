1bb8f433cc4a7c5a453182cae39df8ecf8e71bdb
Martynkin Slavamir <sss_mmm86@mail.ru>
2023-05-23 22:29:03 +0300

    add task64

---
+++ task64/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 65: Задайте значения M и N. Напишите программу, которая 
+//выведет все натуральные числа в промежутке от M до N.
+//M = 1; N = 5 -> "1, 2, 3, 4, 5"
+//M = 4; N = 8 -> "4, 6, 7, 8"
+
+
+int ReadInt(string argument) 
+{
+    Console.Write(argument);
+    int i;
+    while (!int.TryParse(Console.ReadLine(), out i))
+    {
+        System.Console.WriteLine("Это не число!");
+        Console.Write(argument);
+    }
+    return i;
+}
+string NumbersRec1(int m, int n)
+{
+    if (m < n + 1) return $"{m} " + NumbersRec1(m + 1, n);
+    else return String.Empty;
+}
+int m = ReadInt("Введите M: ");
+int n = ReadInt("Введите N: ");
+System.Console.WriteLine(NumbersRec1(m, n));
